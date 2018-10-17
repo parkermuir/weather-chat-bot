c12321475d26cd96030b9f8d33dba7cb29dab682
parkermuir <parker.muir@gmail.com>
2018-10-16 21:29:10 -0400

    Initial api calls to metaweather

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express =  require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 app.use(bodyParser.json());
+
+const urls = {
+  getWoeid: 'https://www.metaweather.com/api/location/search/?query=',
+  getWeather: 'https://www.metaweather.com/api/location/',
+};
 
 const dialogflowResponse = {
   fulfillmentText: '',
@@ -15,7 +21,23 @@ app.post('/weather', ( req, res ) => {
   console.log( { city });
   
   //get location id (woeID) for the city
-
+  axios.get(urls.getWoeid + city)
+    .then( res => {
+      let woeID = res.data[0].woeid;
+      console.log(woeID);
+      return woeid;
+    })
+    .then( woeid => axios.get(urls.getWeather + woied))
+    .then( res => {
+      console.log(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+      let errorResponse = `Oops! The programmer made a mistake. Please try a different query!`;
+      dialogflowResponse.fulfillmentText = errorResponse;
+      response.json(dialogflowResponse);
+    });
+    
   //get temperature from the woeID
 
   //return temperature
